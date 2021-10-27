7af3ea469fa20a266ecb294450eb463d370a2a30
Abhay Singh <abhay5349singh@gmail.com>
2021-10-27 16:46:15 +0530

    Add files via upload

---
+++ Line-Sweep/1589. Maximum Sum Obtained of Any Permutation.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    
+    #define ll long long
+    
+    int maxSumRangeQuery(vector<int> &a, vector<vector<int>> &req) {
+        int n=a.size();
+        vector<ll int> scanline(n+1,0); // storing effect of range occuring in requests
+        
+        for(int i=0;i<req.size();i++){
+            int s=req[i][0], e=req[i][1];
+            scanline[s]++;
+            scanline[e+1]--;
+        }
+        for(int i=1;i<n;i++) scanline[i]+=scanline[i-1];
+        
+        sort(scanline.begin(),scanline.end(),greater<int>());
+        sort(a.begin(),a.end(),greater<int>());
+        
+        ll int sum=0;
+        int mod=1e9+7;
+        
+        for(int i=0;i<n;i++) sum=(sum%mod + (scanline[i]*a[i])%mod)%mod;
+        
+        return (int)sum;
+    }
+};
