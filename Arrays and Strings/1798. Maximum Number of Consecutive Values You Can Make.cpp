470d1720999398bdfb1b4c0945c1c3c12bd352ee
Abhay Singh <abhay5349singh@gmail.com>
2021-10-28 03:14:12 +0530

    Add files via upload

---
+++ Arrays and Strings/1798. Maximum Number of Consecutive Values You Can Make.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int getMaximumConsecutive(vector<int>& a) {
+        sort(a.begin(),a.end());
+        
+        int maxreach=0;
+        int i=0;
+        while(i<a.size()){
+            if(a[i]<=maxreach+1){
+                maxreach+=a[i];
+                i++;
+            }else{
+                break;
+            }
+        }
+        return maxreach+1;
+    }
+};
