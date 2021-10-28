31f7d709f1250984e45733f956f57208a06e9c25
Abhay Singh <abhay5349singh@gmail.com>
2021-10-28 22:17:00 +0530

    Add files via upload

---
+++ Hash-Map and Heaps/974. Subarray Sums Divisible by K.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& a, int k) {
+        int n=a.size();
+        
+        map<int,int> mp;
+        int sum=0;
+        int ans=0;
+        
+        mp[0]=1;
+    /*    for(int i=0;i<n;i++){
+            sum+=a[i];
+            
+            int cur=sum;
+            while(cur<0) cur+=k;
+            
+            int r=(cur)%k;
+            if(mp.find(r)!=mp.end()){
+                ans+=mp[r];
+            }
+            mp[r]++;
+        } */
+        
+        for(int i=0;i<n;i++){
+            sum+=a[i];
+            
+            int r=sum%k;
+            if(r<0) r+=k;
+            if(mp.find(r)!=mp.end()){
+                ans+=mp[r];
+            }
+            mp[r]++;
+        }
+        return ans;
+    }
+};
