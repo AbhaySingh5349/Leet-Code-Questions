0cd6139f9dd5cfd38ff6c7173348a8b3abf103a4
Abhay Singh <abhay5349singh@gmail.com>
2021-10-28 21:22:21 +0530

    Add files via upload

---
+++ Arrays and Strings/905. Sort Array By Parity.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& a) {
+        int n=a.size();
+        int i=0, j=0;
+        while(j<n){
+            while(i<n && a[i]%2==0) i++;
+            j=max(i,j);
+            j++;
+            if(j<n && a[j]%2==0) swap(a[i],a[j]);
+        }
+        return a;
+    }
+};
