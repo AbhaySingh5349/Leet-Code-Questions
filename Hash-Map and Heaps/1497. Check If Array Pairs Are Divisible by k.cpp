598e40c3cf0fdccaddd87f82ac542e82b9b2c39e
Abhay Singh <abhay5349singh@gmail.com>
2021-10-29 03:08:19 +0530

    Add files via upload

---
+++ Hash-Map and Heaps/1497. Check If Array Pairs Are Divisible by k.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool canArrange(vector<int>& a, int k) {
+        int n=a.size();
+        
+        map<int,int> mp;
+        for(int i=0;i<n;i++){
+            int r=a[i]%k;
+            if(r<0) r+=k;
+            mp[r]++;
+        }
+        
+        for(int i=0;i<n;i++){
+            int r=a[i]%k;
+            if(r<0) r+=k;
+            int f=mp[r];
+            
+            if(r==0 || 2*r==k){
+                if(f%2!=0) return false;
+            }else{
+                int cr=k-r;
+                if(mp.find(cr)==mp.end()){
+                    return false;
+                }else{
+                    int cf=mp[cr];
+                    if(f!=cf) return false;
+                }
+            }
+        }
+        return true;
+    }
+};
