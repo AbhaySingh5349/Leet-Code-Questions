f6a4ec0464ebebe529f12bb47f97a0e4f0fe15eb
Abhay Singh <abhay5349singh@gmail.com>
2021-10-30 02:51:42 +0530

    Add files via upload

---
+++ Arrays and Strings/670. Maximum Swap.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int maximumSwap(int num) {
+        vector<int> v;
+        while(num>0){
+            int r=num%10;
+            num/=10;
+            v.push_back(r);
+        }
+        reverse(v.begin(),v.end());
+        
+        int n=v.size();
+        
+        vector<int> right(n);
+        right[n-1]=n-1;
+        
+        for(int i=n-2;i>=0;i--){
+            if(v[i]>v[right[i+1]]){
+                right[i]=i;
+            }else{
+                right[i]=right[i+1];
+            }
+        }
+        
+        for(int i=0;i<n;i++){
+            if(v[right[i]]>v[i]){
+                swap(v[i],v[right[i]]);
+                break;
+            }
+        }
+        
+        int ans=0;
+        for(int i=0;i<n;i++) ans=ans*10+v[i];
+        
+        return ans;
+    }
+};
