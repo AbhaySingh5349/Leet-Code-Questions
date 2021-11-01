82a6950debe129df4e327f126e59df22b597d70d
Abhay Singh <abhay5349singh@gmail.com>
2021-11-01 22:26:40 +0530

    Add files via upload

---
+++ Sorting/1679. Max Number of K-Sum Pairs.cpp
@@ -0,0 +1,59 @@
+Question Link: https://leetcode.com/problems/max-number-of-k-sum-pairs/
+
+//basically we need to find distinct indexed pairs with sum=k
+
+class Solution {
+public:
+    
+    int maxOperations(vector<int>& a, int k) {
+        int n=a.size();
+        
+        sort(a.begin(),a.end());
+        int c=0, i=0, j=n-1;
+        
+        while(i<j){
+            int sum=a[i]+a[j];
+            if(sum<k){
+                i++;
+            }else if(sum>k){
+                j--;
+            }else{
+                c++; // count of disjoint pairs
+                i++,j--;
+            }
+        }
+        return c; 
+    }
+};
+
+// Approach 2:
+
+class Solution {
+public:
+   
+    int maxOperations(vector<int>& a, int k) {
+        int n=a.size();
+        
+        map<int,int> mp;
+        for(int i=0;i<n;i++) mp[a[i]]++;
+        
+        int c=0;
+        map<int,int> :: iterator it;
+        for(it=mp.begin();it!=mp.end();it++){
+            int x=it->first, f=it->second;
+            int y=k-x;
+            if(mp.find(y)!=mp.end()){
+                if(x==y){
+                    c+=f/2;
+                }else{
+                    int val=min(mp[x],mp[y]);
+                    c+=val;
+                    mp[x]-=val;
+                    mp[y]-=val;
+                }
+            }
+        }
+        
+        return c; 
+    }
+};
