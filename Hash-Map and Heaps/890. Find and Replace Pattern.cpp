738119fc893875e8ac8f339aa9778b60022b5bec
Abhay Singh <abhay5349singh@gmail.com>
2021-11-01 15:55:30 +0530

    Add files via upload

---
+++ Hash-Map and Heaps/890. Find and Replace Pattern.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    
+    bool isomorphic(string s, string p){
+        int n=s.length();
+        map<char,char> mp;
+        set<char> st;
+        for(int i=0;i<n;i++){
+            char pc=p[i], sc=s[i];
+            
+            if(mp.find(pc)!=mp.end()){
+                if(sc!=mp[pc]) return false;
+            }else{
+                if(st.find(sc)!=st.end()) return false;
+                mp[pc]=sc;
+                st.insert(sc);
+            }
+        }
+        return true;
+    }
+    
+    vector<string> findAndReplacePattern(vector<string>& words, string p) {
+        vector<string> ans;
+        for(int i=0;i<words.size();i++){
+            string s=words[i];
+            if(p.length()!=s.length()) continue;
+            
+            if(isomorphic(s,p)) ans.push_back(s);
+        }
+        return ans;
+    }
+};
