8be11dd8ee5fbb86624e836abf0b0cf3fe38e79e
Abhay Singh <abhay5349singh@gmail.com>
2021-11-01 10:49:19 +0530

    Add files via upload

---
+++ Linked-List/430. Flatten a Multilevel Doubly Linked List.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    Node* flatten(Node* root) {
+        if(root==NULL) return NULL;
+        
+        Node* head=root;
+        while(head!=NULL){
+            if(head->child!=NULL){
+                Node* on=head->next;
+                
+                Node* newNext=flatten(head->child);
+                
+                head->child=NULL;
+                head->next=newNext;
+                if(newNext) newNext->prev=head;
+                
+                Node* last=newNext;
+                while(last!=NULL && last->next!=NULL) last=last->next;
+                if(last) last->next=on;
+                if(on) on->prev=last;
+            }
+            head=head->next;
+        }
+        return root;
+    }
+};
