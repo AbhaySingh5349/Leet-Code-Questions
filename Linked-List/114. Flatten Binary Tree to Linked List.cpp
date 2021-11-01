780dc29fd4d426ad0393221e0786757b151efefa
Abhay Singh <abhay5349singh@gmail.com>
2021-11-01 11:25:22 +0530

    Update 114. Flatten Binary Tree to Linked List.cpp

--- Linked-List/114. Flatten Binary Tree to Linked List.cpp
+++ Linked-List/114. Flatten Binary Tree to Linked List.cpp
@@ -1,5 +1,7 @@
 Question Link: https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
 
+// Approach 1:
+
 class Solution {
 public:
     
@@ -18,5 +20,29 @@ public:
         TreeNode* cur=root;
         while(cur && cur->right!=NULL) cur=cur->right; // here 'Right Child' is Flattened left child of original tree
         if(cur) cur->right=rc;
+    }
+};
+
+// Approach 2:
+
+class Solution {
+public:
+    
+    void flatten(TreeNode* root) {
+        if(root==NULL) return;
+        
+        while(root!=NULL){
+            if(root->left!=NULL){
+                TreeNode* lc=root->left;
+                TreeNode* ol=root->left;
+                
+                while(lc->right!=NULL) lc=lc->right;
+                
+                lc->right=root->right;
+                root->left=NULL;
+                root->right=ol;
+            }
+            root=root->right;
+        }
     }
 };
