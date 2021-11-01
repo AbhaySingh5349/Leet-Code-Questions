602419e53fefa8fb1df7a9453ac6a50bc32bb37b
Abhay Singh <abhay5349singh@gmail.com>
2021-11-02 03:40:39 +0530

    Add files via upload

---
+++ Hash-Map and Heaps/1296. Divide Array in Sets of K Consecutive Numbers.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isPossibleDivide(vector<int>& a, int k) {
+        int n=a.size();
+        if(n%k) return false;
+        
+        unordered_map<int,int> mp;
+        for(int i=0;i<n;i++) mp[a[i]]++;
+        
+        sort(a.begin(),a.end());
+        int i=0;
+        while(i<n){
+            int x=a[i];
+            if(mp.find(x)==mp.end()){
+                i++;
+                continue;
+            } 
+            
+            int c=k;
+            while(mp.find(x)!=mp.end() && c!=0){
+                mp[x]--;
+                if(mp[x]==0) mp.erase(x);
+                x++;
+                c--;
+            }
+            if(c!=0) return false;
+        }
+        return true;
+    }
+};
