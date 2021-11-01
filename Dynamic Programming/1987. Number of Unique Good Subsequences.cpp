3d20bdb3fbebabe527b07464881f5b2484b62fc7
Abhay Singh <abhay5349singh@gmail.com>
2021-11-02 00:38:21 +0530

    Add files via upload

---
+++ Dynamic Programming/1987. Number of Unique Good Subsequences.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    
+    #define ll long long
+    int mod=1e9+7;
+    
+    int numberOfUniqueGoodSubsequences(string s) {
+        int n=s.length();
+        
+        int i=0, c0=0;
+        while(i<n && s[i]=='0') i++, c0++;
+        if(i==n) return 1;
+        
+        ll int dp[n];
+        memset(dp,0,sizeof(dp));
+        dp[i]=1;
+        
+        int idx0=-1, idx1=i;
+        i++;
+        while(i<n){
+            dp[i]=(2*dp[i-1])%mod;
+            
+            if(s[i]=='0'){
+                if(idx0!=-1) dp[i]= (dp[i]%mod - dp[idx0-1]%mod + mod)%mod;
+                idx0=i;
+                c0++;
+            }else{
+                if(idx1!=0) dp[i]= (dp[i]%mod - dp[idx1-1]%mod + mod)%mod;
+                idx1=i;
+            }
+            i++;
+        }
+        return dp[n-1]+(c0!=0);
+    }
+};
