2c9f891dddfade9ea83ab1df93fa6a0cf1488147
Abhay Singh <abhay5349singh@gmail.com>
2021-11-03 01:22:07 +0530

    Add files via upload

---
+++ Maths/62. Unique Paths.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    
+    #define ll long long
+    
+    ll int ncr(int n, int r){
+        if(n<r) return 0;
+        ll int ans=1L;
+        
+        for(int i=0;i<r;i++){
+            ans*=(n-i);
+            ans/=(i+1);
+        }
+        return ans;
+    }
+    
+    int uniquePaths(int m, int n) {
+        return (int)ncr(m+n-2,min(n,m)-1);
+    }
+};
