d9e6505fcd83f4050d11524ab49ac4be091301e6
Abhay Singh <abhay5349singh@gmail.com>
2021-11-03 15:01:17 +0530

    Add files via upload

---
+++ Hash-Map and Heaps/1577. Number of Ways Where Square of Number Is Equal to Product of Two Numbers.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    
+    #define ll long long
+    
+    int numTriplets(vector<int> &a, vector<int> &b) {
+        int n=a.size(), m=b.size();
+        
+        map<ll int,int> ma, mb;
+        
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                ll int p=(long)a[i]*a[j];
+                ma[p]++;
+            }
+        }
+        
+        for(int i=0;i<m-1;i++){
+            for(int j=i+1;j<m;j++){
+                ll int p=(long)b[i]*b[j];
+                mb[p]++;
+            }
+        }
+        
+        int ans=0;
+        
+        for(int i=0;i<n;i++){
+            ll int p=(long)a[i]*a[i];
+            if(mb.find(p)!=mb.end()) ans+=mb[p];
+        }
+        
+        for(int i=0;i<m;i++){
+            ll int p=(long)b[i]*b[i];
+            if(ma.find(p)!=ma.end()) ans+=ma[p];
+        }
+        return ans;
+    }
+};
