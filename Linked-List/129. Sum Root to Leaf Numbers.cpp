9d4225c0b0ec8d8641f32f3703b39a0a44bec5c6
Abhay Singh <abhay5349singh@gmail.com>
2021-11-05 13:42:06 +0530

    Add files via upload

---
+++ Linked-List/129. Sum Root to Leaf Numbers.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    
+    void solve(TreeNode* root, int cur, int &ans){
+        if(root==NULL) return;
+        
+        cur=cur*10+(root->val);
+        if(root->left==NULL && root->right==NULL){
+            ans+=cur;
+            return;
+        }
+        
+        solve(root->left,cur,ans);
+        solve(root->right,cur,ans);
+    }
+    
+    int sumNumbers(TreeNode* root) {
+        int ans=0;
+        solve(root,0,ans);
+        return ans;
+    }
+};
