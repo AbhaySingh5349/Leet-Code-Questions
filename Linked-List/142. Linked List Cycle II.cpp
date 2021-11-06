8535eb792a038be6e10d4a807458eaae39cdafd2
Abhay Singh <abhay5349singh@gmail.com>
2021-11-06 20:57:25 +0530

    Add files via upload

---
+++ Linked-List/142. Linked List Cycle II.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if(head==NULL) return NULL;
+        
+        ListNode *fast, *slow;
+        fast=slow=head;
+        
+        do{
+            slow=slow->next;
+            fast=fast->next;
+            if(fast) fast=fast->next;
+        }while(slow!=fast && fast!=NULL && slow!=fast);
+        
+        if(fast==NULL) return NULL;
+        
+        int len=1;
+        while(slow->next!=fast){
+            len++;
+            slow=slow->next;
+        }
+        
+        slow=fast=head;
+        for(int i=0;i<len;i++) fast=fast->next;
+        
+        while(slow!=fast){
+            slow=slow->next;
+            fast=fast->next;
+        }
+        return slow;
+    }
+};
