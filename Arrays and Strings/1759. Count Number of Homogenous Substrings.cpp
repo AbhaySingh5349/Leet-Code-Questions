59a3e83f0a0fe093c2bd4fb46688d662e6a470c9
Abhay Singh <abhay5349singh@gmail.com>
2021-11-08 03:08:41 +0530

    Add files via upload

---
+++ Arrays and Strings/1759. Count Number of Homogenous Substrings.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    
+    #define ll long long
+    int mod=1e9+7;
+    
+    int countHomogenous(string s) {
+        int n=s.length();
+        
+        ll int i=-1, j=1;
+        ll int ans=1;
+        while(j<n){
+            if(s[j]!=s[j-1]){
+                i=j-1;
+                ans++;
+            }else{
+                ans+=(j-i);
+            }
+            ans%=mod;
+            j++;
+        }
+        return (int)ans;
+    }
+};
