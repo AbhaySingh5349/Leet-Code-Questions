eb9bde4fcf7d51bfe5107d21bc062f346ce3ec5d
Abhay Singh <abhay5349singh@gmail.com>
2021-11-10 01:19:35 +0530

    Add files via upload

---
+++ Dynamic Programming/978. Longest Turbulent Subarray.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxTurbulenceSize(vector<int>& a) {
+        int n=a.size();
+        
+        vector<int> cs(n,1), cb(n,1);
+        int maxlen=1;
+        for(int i=1;i<n;i++){
+            if(a[i]>a[i-1]){
+                cb[i]=cs[i-1]+1;
+            }else if(a[i]<a[i-1]){
+                cs[i]=cb[i-1]+1;
+            }else{
+                cs[i]=cb[i]=1;
+            }
+            
+            maxlen=max(maxlen,max(cs[i],cb[i]));
+        }
+        return maxlen;
+    }
+};
