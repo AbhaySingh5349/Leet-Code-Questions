97d4e90ef4b6b228d3fb0d1f2c246a4a3dd8b4ad
Abhay Singh <abhay5349singh@gmail.com>
2021-11-09 22:46:14 +0530

    Update 1685. Sum of Absolute Differences in a Sorted Array.cpp

--- Arrays and Strings/1685. Sum of Absolute Differences in a Sorted Array.cpp
+++ Arrays and Strings/1685. Sum of Absolute Differences in a Sorted Array.cpp
@@ -1,5 +1,15 @@
 Question Link: https://leetcode.com/problems/sum-of-absolute-differences-in-a-sorted-array/
 
+1. Considering Left --> Right movement
+
+let i=number of element to left of current
+since the array is sorted, increment in current difference will be i*(a[i]-a[i-1])
+    
+2.Considering Right --> Left movement
+
+let (n-i-1)=number of element to right of current
+since the array is sorted, increment in current difference will be (n-i-1)*(a[i+1]-a[i])
+
 class Solution {
 public:
     vector<int> getSumAbsoluteDifferences(vector<int>& a) {
