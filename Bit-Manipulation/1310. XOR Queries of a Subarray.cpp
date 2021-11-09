9973ba5f11f7db9a04ca1ea193a4057e69e58c2a
Abhay Singh <abhay5349singh@gmail.com>
2021-11-09 17:27:17 +0530

    Add files via upload

---
+++ Bit-Manipulation/1310. XOR Queries of a Subarray.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> xorQueries(vector<int>& a, vector<vector<int>>& queries) {
+        int n=a.size();
+        
+        vector<int> pre(n,0);
+        pre[0]=a[0];
+        for(int i=1;i<n;i++) pre[i]=a[i]^pre[i-1];
+        
+        int m=queries.size();
+        vector<int> ans(m);
+        for(int i=0;i<m;i++){
+            int l=queries[i][0], r=queries[i][1]; 
+            int val=(pre[r]^(l>0 ? pre[l-1]:0));
+            ans[i]=val;
+        } 
+        return ans;
+    }
+};
