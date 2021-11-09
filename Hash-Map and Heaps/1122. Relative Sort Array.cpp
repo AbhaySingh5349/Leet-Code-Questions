d0e837336112a4976b4b04c937baa9384bbcc306
Abhay Singh <abhay5349singh@gmail.com>
2021-11-09 17:30:25 +0530

    Add files via upload

---
+++ Hash-Map and Heaps/1122. Relative Sort Array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> relativeSortArray(vector<int>& a, vector<int>& b) {
+        int n=a.size(), m=b.size();
+        
+        map<int,int> mp;
+        for(int i=0;i<n;i++) mp[a[i]]++;
+        
+        vector<int> ans;
+        for(int i=0;i<m;i++){
+            int f=mp[b[i]];
+            ans.insert(ans.end(),f,b[i]);
+            mp.erase(b[i]);
+        }
+        
+        map<int,int> :: iterator it;
+        for(it=mp.begin();it!=mp.end();it++){
+            int f=it->second, val=it->first;
+            ans.insert(ans.end(),f,val);
+        }
+        return ans;
+    }
+};
