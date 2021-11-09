7562dda7c2bda720d51ef98e4b2b8d474bad455a
Abhay Singh <abhay5349singh@gmail.com>
2021-11-09 20:51:57 +0530

    Add files via upload

---
+++ Design/1352. Product of the Last K Numbers.cpp
@@ -0,0 +1,29 @@
+class ProductOfNumbers {
+public:
+    
+    vector<int> p;
+    int idx;
+    
+    ProductOfNumbers() {
+        p.push_back(1);
+        idx=-1;
+    }
+    
+    void add(int num) {
+        if(num==0 || p.back()==0){
+            if(num==0) idx=p.size();
+            p.push_back(num);
+        }else{
+            p.push_back(p.back()*num);
+        }
+    }
+    
+    int getProduct(int k) {
+        int limit=p.size()-k;
+        if(idx>=limit) return 0;
+        
+        if(p[limit-1]==0) return p.back(); // to avoid divide by zero
+        
+        return (p.back()/p[limit-1]);
+    }
+};
