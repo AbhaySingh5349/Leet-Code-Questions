0d6f69916976c26bc019e568c11462200edf9dfe
Abhay Singh <abhay5349singh@gmail.com>
2021-11-10 18:30:55 +0530

    Add files via upload

---
+++ Arrays and Strings/1616. Split Two Strings to Make Palindrome.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    
+    bool palindrome(string s, int i, int j){
+        while(i<j){
+            if(s[i]!=s[j]) return false;
+            i++, j--;
+        }
+        return true;
+    }
+    
+    bool check(string a, string b){
+        int n=a.length();
+        
+        int i=0, j=n-1;
+        while(i<j){
+            if(a[i]!=b[j]) return palindrome(a,i,j) || palindrome(b,i,j);
+            i++, j--;
+        }
+        return true;
+    }
+    
+    bool checkPalindromeFormation(string a, string b) {
+        
+        return check(a,b) || check(b,a);
+    }
+};
