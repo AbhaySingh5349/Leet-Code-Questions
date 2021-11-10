329c4b093b391437c0b74dace7e17726a43748a2
Abhay Singh <abhay5349singh@gmail.com>
2021-11-11 00:06:47 +0530

    Add files via upload

---
+++ Hash-Map and Heaps/502. IPO.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findMaximizedCapital(int k, int w, vector<int>& profit, vector<int>& capital) {
+        int n=capital.size();
+        
+        priority_queue<pair<int,int>> ppq;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> cpq;
+        
+        for(int i=0;i<n;i++) cpq.push({capital[i],i});
+        
+        while(k>0){
+            while(cpq.size()>0 && cpq.top().first<=w){
+                int idx=cpq.top().second;
+                cpq.pop();
+                
+                ppq.push({profit[idx],idx});
+            }
+            if(ppq.size()==0) break;
+            
+            w+=ppq.top().first;
+            ppq.pop();
+            k--;
+        }
+        return w;
+    }
+};
