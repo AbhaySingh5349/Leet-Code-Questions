a8dad5f0062f35b3b08407e4013568de97db3a25
Abhay Singh <abhay5349singh@gmail.com>
2021-11-10 17:09:28 +0530

    Add files via upload

---
+++ Arrays and Strings/1663. Smallest String With A Given Numeric Value.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    
+    string getSmallestString(int n, int k) {
+        string ans = string(n, 'a');
+        
+        k-=n;
+        int idx=n-1;
+        while(k>0){
+            ans[idx]+=min(25,k);
+            k-=min(25,k);
+            idx--;
+        }
+
+        return ans;
+    }
+};
