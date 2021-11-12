2a4d194b6e882854c95ddbc7262fd22de74ef8cf
Abhay Singh <abhay5349singh@gmail.com>
2021-11-12 10:36:42 +0530

    Update 538. Convert BST to Greater Tree.cpp

--- Binary-Search-Tree/538. Convert BST to Greater Tree.cpp
+++ Binary-Search-Tree/538. Convert BST to Greater Tree.cpp
@@ -1,14 +1,5 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+Question Link: https://leetcode.com/problems/convert-bst-to-greater-tree/
+
 class Solution {
 public:
     
