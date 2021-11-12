92bf152194066a0109b7cc7eafd6a0abbc9c394d
Abhay Singh <abhay5349singh@gmail.com>
2021-11-12 12:51:06 +0530

    Add files via upload

---
+++ Arrays and Strings/841. Keys and Rooms.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool canVisitAllRooms(vector<vector<int>>& a) {
+        int n=a.size();
+        
+        set<int> visited;
+        visited.insert(0);
+        
+        queue<int> q;
+        q.push(0);
+        
+        while(q.size()>0){
+            int idx=q.front();
+            q.pop();
+            
+            for(int i=0;i<a[idx].size();i++){
+                int key=a[idx][i];
+                if(visited.find(key)==visited.end()){
+                    visited.insert(key);
+                    q.push(key);
+                }
+            }
+        }
+        return (visited.size()==n);
+    }
+};
