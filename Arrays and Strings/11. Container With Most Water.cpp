fbc43bc07ef7717c6ed2c0985b964df36b722e12
Abhay Singh <abhay5349singh@gmail.com>
2021-12-10 13:21:38 +0530

    Update 11. Container With Most Water.cpp

--- Arrays and Strings/11. Container With Most Water.cpp
+++ Arrays and Strings/11. Container With Most Water.cpp
@@ -1,5 +1,7 @@
 Link: https://leetcode.com/problems/container-with-most-water/
 
+// Approach 1:
+
 class Solution {
 	public:
 		int maxArea(vector<int>& a) {
@@ -7,14 +9,29 @@ class Solution {
 
 			int ans=0;
 
-			/*    for(int i=0;i<n;i++){
+			    for(int i=0;i<n;i++){
 			        int h=a[i], r=i, l=i;
 
 			        for(int j=i+1;j<n;j++) if(a[j]>=a[i]) r=j;
 			        for(int j=i-1;j>=0;j--) if(a[j]>=a[i]) l=j;
 
 			        ans=max(ans,h*(r-l));
-			    } */
+			    } 
+
+			return ans;
+		}
+};
+
+// Approach 2:
+
+Link: https://leetcode.com/problems/container-with-most-water/
+
+class Solution {
+	public:
+		int maxArea(vector<int>& a) {
+			int n=a.size();
+
+			int ans=0;
 
 			int i=0, j=n-1;
 			while(i<j) {
