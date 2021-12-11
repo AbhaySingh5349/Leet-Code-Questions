23a999639b2de06e30590168d834f585f91d661e
Abhay Singh <abhay5349singh@gmail.com>
2021-12-11 19:45:20 +0530

    Add files via upload

---
+++ Arrays and Strings/121. Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,20 @@
+Question Link: https://leetcode.com/problems/insert-interval/
+
+class Solution {
+public:
+    int maxProfit(vector<int> &a) {
+        int n=a.size();
+        
+        vector<int> right(n);
+        right[n-1]=a[n-1];
+        for(int i=n-2;i>=0;i--) right[i]=max(right[i+1],a[i]);
+        
+        int ans=0;
+        int left=a[0];
+        for(int i=0;i<n-1;i++){
+            left=min(left,a[i]);
+            ans=max(ans,right[i+1]-left);
+        }
+        return ans;
+    }
+};
