7017b5f7e1647ecca07e03a46296a1f2eb0e4679
Abhay Singh <abhay5349singh@gmail.com>
2021-12-11 19:50:01 +0530

    Update 121. Best Time to Buy and Sell Stock.cpp

--- Arrays and Strings/121. Best Time to Buy and Sell Stock.cpp
+++ Arrays and Strings/121. Best Time to Buy and Sell Stock.cpp
@@ -1,5 +1,7 @@
 Question Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 
+// Approach 1:
+
 class Solution {
 public:
     int maxProfit(vector<int> &a) {
@@ -14,6 +16,23 @@ public:
         for(int i=0;i<n-1;i++){
             left=min(left,a[i]);
             ans=max(ans,right[i+1]-left);
+        }
+        return ans;
+    }
+};
+
+// Approach 2:
+
+class Solution {
+public:
+    int maxProfit(vector<int> &a) {
+        int n=a.size();
+        
+        int ans=0;
+        int right=a[n-1];
+        for(int i=n-2;i>=0;i--){
+            ans=max(ans,right-a[i]);
+            right=max(right,a[i]);
         }
         return ans;
     }
