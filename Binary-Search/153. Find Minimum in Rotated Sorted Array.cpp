94673fef8b3a2fb5efd15e73a749a57cb5777777
Abhay Singh <abhay5349singh@gmail.com>
2021-12-13 00:57:26 +0530

    Update 153. Find Minimum in Rotated Sorted Array.cpp

--- Binary-Search/153. Find Minimum in Rotated Sorted Array.cpp
+++ Binary-Search/153. Find Minimum in Rotated Sorted Array.cpp
@@ -1,5 +1,7 @@
 Question Link: https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
 
+// Approach 1:
+
 class Solution {
 public:
     int findMin(vector<int>& a) {
@@ -13,10 +15,34 @@ public:
             if(a[m]<a[(m+1)%n] && a[m]<a[(m-1+n)%n]) return a[m];
             
             if(a[m]>a[n-1]){
-                // left side of peak
+                l=m+1; // left side of peak
+            }else{
+                r=m-1; // right side of peak
+            }
+        }
+        return -1;
+    }
+};
+
+// Approach 2:
+
+class Solution {
+public:
+    
+    int findMin(vector<int> &a) {
+        int n=a.size();
+        if(a[0]<a[n-1] || n==1) return a[0];
+        
+        int l=0, r=n-1;
+        
+        while(l<=r){
+            int m=l+(r-l)/2;
+            
+            if(a[m]<a[(m+1)%n] && a[m]<a[(m-1+n)%n]) return a[m];
+            
+            if(a[m]>=a[0]){
                 l=m+1;
             }else{
-                // right side of peak
                 r=m-1;
             }
         }
