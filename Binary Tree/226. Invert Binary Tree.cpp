a19dab61f1b24df680d75ab20d80fc82dcaa6f54
Abhay Singh <abhay5349singh@gmail.com>
2021-12-13 11:08:23 +0530

    Update 226. Invert Binary Tree.cpp

--- Binary Tree/226. Invert Binary Tree.cpp
+++ Binary Tree/226. Invert Binary Tree.cpp
@@ -1,5 +1,7 @@
 Question Link: https://leetcode.com/problems/invert-binary-tree/
 
+// Approach 1:
+
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
@@ -11,6 +13,36 @@ public:
         root->left=nr;
         root->right=nl;
         
+        return root;
+    }
+};
+
+// Approach 2:
+
+class Solution {
+public:
+    
+    TreeNode* invertTree(TreeNode* root) {
+        if(root==NULL) return NULL;
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        while(q.size()>0){
+            int size=q.size();
+            while(size--){
+                TreeNode *cur=q.front();
+                q.pop();
+                
+                TreeNode *nl=cur->right, *nr=cur->left;
+                
+                if(nr!=NULL) q.push(nr);
+                if(nl!=NULL) q.push(nl);
+                
+                cur->left=nl;
+                cur->right=nr;
+            } 
+        }
         return root;
     }
 };
