fd4ee45a6f7459ed332e17f69e98339a3ac7f43c
Abhay Singh <abhay5349singh@gmail.com>
2021-12-17 12:31:55 +0530

    Update 421. Maximum XOR of Two Numbers in an Array.cpp

--- Trie/421. Maximum XOR of Two Numbers in an Array.cpp
+++ Trie/421. Maximum XOR of Two Numbers in an Array.cpp
@@ -1,4 +1,4 @@
-Question Link: https://leetcode.com/problems/find-subsequence-of-length-k-with-the-largest-sum/
+Question Link: https://leetcode.com/problems/maximum-xor-of-two-numbers-in-an-array/
 
 class Solution {
 public:
