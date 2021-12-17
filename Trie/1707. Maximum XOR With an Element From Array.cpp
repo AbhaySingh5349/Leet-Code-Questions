c31f18c49498a17aa42ddcb0bc82994a10897e5e
Abhay Singh <abhay5349singh@gmail.com>
2021-12-17 16:08:09 +0530

    Update 1707. Maximum XOR With an Element From Array.cpp

--- Trie/1707. Maximum XOR With an Element From Array.cpp
+++ Trie/1707. Maximum XOR With an Element From Array.cpp
@@ -1,4 +1,4 @@
-Question Link: https://leetcode.com/problems/find-subsequence-of-length-k-with-the-largest-sum/
+Question Link: https://leetcode.com/problems/maximum-xor-with-an-element-from-array/
 
 class Solution {
 public:
