45d82e5434a3f3d1ded084b0e968875aefa25fbc
Abhay Singh <abhay5349singh@gmail.com>
2021-12-18 23:19:32 +0530

    Add files via upload

---
+++ Trie/720. Longest Word in Dictionary.cpp
@@ -0,0 +1,25 @@
+Question Link: https://leetcode.com/problems/find-subsequence-of-length-k-with-the-largest-sum/
+
+class Solution {
+public:
+    string longestWord(vector<string>& words) {
+        sort(words.begin(),words.end());
+        
+        int n=words.size();
+        string ans="", cur="";
+        int maxlen=0;
+        
+        set<string> st;
+        for(int i=0;i<n;i++){
+            string s=words[i];
+            cout<<s<<"  ";
+            string sub=s.substr(0,s.length()-1);
+            
+            if(s.length()==1 || st.find(sub)!=st.end()){
+                if(s.length()>ans.length()) ans=s;
+                st.insert(s);
+            }
+        }
+        return ans;
+    }
+};
