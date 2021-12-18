1e0e717bc568e7347de7985dd46b1ac0cd2a7f77
Abhay Singh <abhay5349singh@gmail.com>
2021-12-18 23:20:46 +0530

    Update 720. Longest Word in Dictionary.cpp

--- Trie/720. Longest Word in Dictionary.cpp
+++ Trie/720. Longest Word in Dictionary.cpp
@@ -27,3 +27,60 @@ public:
 };
 
 // Approach 2:
+
+class Solution {
+public:
+    
+    struct TrieNode{
+        char ch;
+        TrieNode *child[26];
+        int wordEnd;
+    };
+    
+    TrieNode *root;
+    
+    TrieNode *create(int idx){
+        TrieNode *cur = new TrieNode;
+        cur->ch = 'a' + idx;
+        for(int i=0;i<26;i++) cur->child[i]=NULL;
+        cur->wordEnd=0;
+        
+        return cur;
+    }
+    
+    void insert(string s){
+        TrieNode *cur = root;
+        for(int i=0;i<s.length();i++){
+            int idx=s[i]-'a';
+            if(cur->child[idx]==NULL) cur->child[idx]=create(idx);
+            cur=cur->child[idx];
+        }
+        cur->wordEnd++;
+    }
+    
+    bool search(string s){
+        TrieNode *cur = root;
+        for(int i=0;i<s.length();i++){
+            int idx=s[i]-'a';
+            if(cur->child[idx]==NULL || cur->child[idx]->wordEnd==0) return false;
+            cur=cur->child[idx];
+        }
+        return true;
+    }
+    
+    string longestWord(vector<string>& words) {
+        sort(words.begin(),words.end());
+        
+        int n=words.size();
+        
+        string ans="";
+        
+        root = create('*'-'a');
+        for(int i=0;i<n;i++) insert(words[i]);
+        
+        for(int i=0;i<n;i++){
+            if((words[i].length()==1 || search(words[i])) && words[i].length()>ans.length()) ans=words[i];
+        }
+        return ans;
+    }
+};
