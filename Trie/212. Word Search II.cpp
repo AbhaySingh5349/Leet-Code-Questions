19263af35abee2aefa4bb4f0b9f7bb42a8da4cce
Abhay Singh <abhay5349singh@gmail.com>
2021-12-24 11:22:52 +0530

    Add files via upload

---
+++ Trie/212. Word Search II.cpp
@@ -0,0 +1,99 @@
+Question Link: https://leetcode.com/problems/find-subsequence-of-length-k-with-the-largest-sum/
+
+class Solution {
+public:
+    
+    struct TrieNode{
+        char ch;
+        TrieNode *child[26];
+        int wordEnd;
+        int index; // original index of word ending at this node 
+        int count;
+    };
+    
+    TrieNode *root;
+    
+    TrieNode *createNode(int idx){
+        TrieNode *cur = new TrieNode;
+        cur->ch = 'a'+idx;
+        for(int i=0;i<26;i++) cur->child[i]=NULL;
+        cur->wordEnd=0, cur->count=0;
+        cur->index=-1;
+        
+        return cur;
+    }
+    
+    void insert(string s, int k){
+        TrieNode* cur=root;
+        for(int i=0;i<s.length();i++){
+            int idx=s[i]-'a';
+            if(cur->child[idx]==NULL) cur->child[idx]=createNode(idx);
+            
+            cur->count++;
+            cur=cur->child[idx];
+        }
+        cur->wordEnd++;
+        cur->index=k;
+    }
+    
+    void erase(string s) { // pruning step
+        TrieNode* cur=root;
+        for(int i=0;i<s.length();i++){
+            char ch=s[i];
+            int idx=ch-'a';
+            if(cur->child[idx]==NULL) cur->child[idx]=createNode(idx);
+            
+            cur->count--;
+            cur=cur->child[idx];
+        }
+        cur->wordEnd--;
+    }
+    
+    int dxy[4][2]={{-1,0},{0,1},{1,0},{0,-1}};
+    void solve(vector<vector<char>>& grid, int i, int j, TrieNode *cur, set<int> &st, vector<string> &words){
+        int n=grid.size(), m=grid[0].size();
+        if(i<0 || j<0 || i==n || j==m || grid[i][j]=='*') return ;
+        
+        int idx=grid[i][j]-'a';
+        if(cur->child[idx]==NULL) return;
+        
+        cur=cur->child[idx];
+        
+        TrieNode* temp=cur;
+        if(cur->wordEnd>0 && cur->index!=-1){
+            st.insert(cur->index);
+            erase(words[cur->index]);
+        }
+        
+        if(cur->count==0) return;
+        
+        char ch=grid[i][j];
+        grid[i][j]='*';
+        for(int k=0;k<4;k++){
+            int x=i+dxy[k][0], y=j+dxy[k][1];
+            solve(grid,x,y,temp,st,words);
+        }
+        grid[i][j]=ch;
+    }
+    
+    vector<string> findWords(vector<vector<char>>& grid, vector<string>& words) {
+        root = createNode('*'-'a');
+        
+        for(int i=0;i<words.size();i++) insert(words[i],i);
+        
+        int n=grid.size(), m=grid[0].size();
+        
+        set<int> st;
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                solve(grid,i,j,root,st,words);
+            }
+        }
+        vector<string> ans;
+        
+        set<int> :: iterator it;
+        for(it=st.begin();it!=st.end();it++) ans.push_back(words[*it]);
+        return ans;
+    }
+};
