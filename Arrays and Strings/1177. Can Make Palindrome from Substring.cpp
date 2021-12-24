1988d9a55fb5bb6d829adfe2003359238cd54dd1
Abhay Singh <abhay5349singh@gmail.com>
2021-12-24 23:27:54 +0530

    Update 1177. Can Make Palindrome from Substring.cpp

--- Arrays and Strings/1177. Can Make Palindrome from Substring.cpp
+++ Arrays and Strings/1177. Can Make Palindrome from Substring.cpp
@@ -46,3 +46,40 @@ public:
         return ans;
     }
 };
+
+// Approach 2:
+
+class Solution {
+public:
+    vector<bool> canMakePaliQueries(string s, vector<vector<int>>& queries) {
+        int n=queries.size();
+        vector<bool> ans(n,false);
+        
+        vector<vector<int>> dp;
+        vector<int> freq(26,0);
+        
+        for(int i=0;i<s.length();i++){
+            freq[s[i]-'a']++;
+            dp.push_back(freq);
+        }
+        
+        for(int k=0;k<n;k++){
+            int i=queries[k][0], j=queries[k][1], allowed=queries[k][2];
+            
+            vector<int> low(26,0), high(26,0);
+            
+            if(i>0) low=dp[i-1];
+            high=dp[j]; 
+            
+            int odd=0;
+            for(int idx=0;idx<26;idx++){
+                int f=high[idx]-low[idx];
+                odd += (f%2 != 0);
+            }
+            
+            ans[k] = (odd/2<=allowed);
+        }
+        
+        return ans;
+    }
+};
