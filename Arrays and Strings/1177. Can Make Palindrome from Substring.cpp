5295e22a974399084913c69ea18ff98b45af2847
Abhay Singh <abhay5349singh@gmail.com>
2021-12-24 23:27:16 +0530

    Add files via upload

---
+++ Arrays and Strings/1177. Can Make Palindrome from Substring.cpp
@@ -0,0 +1,48 @@
+Question Link: https://leetcode.com/problems/find-subsequence-of-length-k-with-the-largest-sum/
+
+class Solution {
+public:
+    vector<bool> canMakePaliQueries(string s, vector<vector<int>>& queries) {
+        int n=queries.size();
+        vector<bool> ans(n,false);
+        
+    /*    for(int k=0;k<n;k++){
+            int i=queries[k][0], j=queries[k][1], allowed=queries[k][2];
+            
+            vector<int> freq(26,0);
+            for(int idx=i;idx<=j;idx++) freq[s[idx]-'a']++;
+            
+            int odd=0;
+            for(int idx=0;idx<26;idx++) odd += (freq[idx]%2 != 0);
+            
+            ans[k] = (odd/2<=allowed);
+        } */
+        
+        vector<vector<int>> dp;
+        vector<int> freq(26,0);
+        
+        for(int i=0;i<s.length();i++){
+            freq[s[i]-'a']++;
+            dp.push_back(freq);
+        }
+        
+        for(int k=0;k<n;k++){
+            int i=queries[k][0], j=queries[k][1], allowed=queries[k][2];
+            
+            vector<int> low(26,0), high(26,0);
+            
+            if(i>0) low=dp[i-1];
+            high=dp[j]; 
+            
+            int odd=0;
+            for(int idx=0;idx<26;idx++){
+                int f=high[idx]-low[idx];
+                odd += (f%2 != 0);
+            }
+            
+            ans[k] = (odd/2<=allowed);
+        }
+        
+        return ans;
+    }
+};
