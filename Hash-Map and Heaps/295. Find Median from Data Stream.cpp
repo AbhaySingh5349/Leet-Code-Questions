1a09a56d23c6d7716798d27f33ee0dd15fb6795d
Abhay Singh <abhay5349singh@gmail.com>
2021-12-25 17:40:50 +0530

    Update 295. Find Median from Data Stream.cpp

--- Hash-Map and Heaps/295. Find Median from Data Stream.cpp
+++ Hash-Map and Heaps/295. Find Median from Data Stream.cpp
@@ -1,5 +1,7 @@
 Question Link: https://leetcode.com/problems/find-subsequence-of-length-k-with-the-largest-sum/
 
+// Approach 1:
+
 class MedianFinder {
 public:
     
@@ -28,6 +30,70 @@ public:
             ans=v[m];
         }else{
             ans = (v[m]+v[m+1])/2.0;
+        }
+        return ans;
+    }
+};
+
+// Approach 2:
+
+class MedianFinder {
+public:
+    
+    priority_queue<int> maxpq; // left half
+    priority_queue<int,vector<int>,greater<int>> minpq; // right half
+    
+    MedianFinder() {
+        maxpq = priority_queue<int>();
+        minpq = priority_queue<int,vector<int>,greater<int>>();
+    }
+    
+    void addNum(int num) {
+        int n=maxpq.size(), m=minpq.size();
+        
+        if(n==0){ // 1st element
+            maxpq.push(num);
+        }else if(n==m){
+            if(num < minpq.top()){
+                maxpq.push(num);
+            }else{
+                int top=minpq.top();
+                minpq.pop();
+                
+                minpq.push(num);
+                maxpq.push(top);
+            }
+        }else if(n==m+1){
+            if(m==0){
+                if(num>maxpq.top()){
+                    minpq.push(num);
+                }else{
+                    int top=maxpq.top();
+                    maxpq.pop();
+
+                    maxpq.push(num);
+                    minpq.push(top);
+                }
+            }else if(num<=maxpq.top()){
+                int top=maxpq.top();
+                maxpq.pop();
+                
+                maxpq.push(num);
+                minpq.push(top);
+            }else{
+                minpq.push(num);
+            }
+        }
+    }
+    
+    double findMedian() {
+        double ans=0;
+        
+        int n=maxpq.size(), m=minpq.size();
+        if(n==m){
+            ans = (minpq.top() + maxpq.top())/2.0;
+        }else{
+            ans=maxpq.top();
         }
         return ans;
     }
