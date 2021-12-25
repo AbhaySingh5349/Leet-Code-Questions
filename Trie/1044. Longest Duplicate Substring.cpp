8552d1c6b9ec40069183795afa2ac37834e7691f
Abhay Singh <abhay5349singh@gmail.com>
2021-12-25 12:52:31 +0530

    Add files via upload

---
+++ Trie/1044. Longest Duplicate Substring.cpp
@@ -0,0 +1,60 @@
+Question Link: https://leetcode.com/problems/find-subsequence-of-length-k-with-the-largest-sum/
+
+class Solution {
+public:
+    
+    struct TrieNode{
+        char ch;
+        TrieNode *child[26];
+        TrieNode(int idx){
+            this->ch = 'a'+idx;
+            for(int i=0;i<26;i++) this->child[i]=NULL;
+        }
+    };
+    
+    TrieNode *root;
+    
+    bool insert(string s){
+        TrieNode *cur = root;
+        bool possible=true;
+        for(int i=0;i<s.length();i++){
+            int idx=s[i]-'a';
+            if(cur->child[idx]==NULL){
+                possible=false;
+                cur->child[idx]=new TrieNode(idx);
+            }
+            cur=cur->child[idx];
+        }
+        return possible;
+    }
+    
+    string longestDupSubstring(string s) {
+        string ans="";
+        int n=s.length();
+        
+        int l=1, r=n-1;
+        while(l<r){
+            int len=l+(r-l+1)/2;
+            
+            root = new TrieNode('*'-'a');
+            bool found=false;
+            for(int i=0;i<n-len+1;i++){
+                int j=i+len-1;
+                string sub="";
+                for (int k=i;k<=j;k++) sub+=s[k];
+                if(insert(sub)){
+                    ans=sub;
+                    found=true;
+                    break;
+                }
+            }
+            
+            if(found){
+                l=len;
+            }else{
+                r=len-1;
+            }
+        }
+        return ans;
+    }
+};
