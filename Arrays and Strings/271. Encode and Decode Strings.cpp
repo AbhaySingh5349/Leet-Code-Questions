dad179e33719eb5f0f6ebbc1f18c323ec85350b8
Abhay Singh <abhay5349singh@gmail.com>
2021-12-25 14:54:49 +0530

    Update 271. Encode and Decode Strings.cpp

--- Arrays and Strings/271. Encode and Decode Strings.cpp
+++ Arrays and Strings/271. Encode and Decode Strings.cpp
@@ -1,36 +1,38 @@
 Question Link: https://leetcode.com/problems/encode-and-decode-strings/
 
-class Solution {
+class Codec {
 public:
-  
-    string encode(vector<string> &v) {
+
+    // Encodes a list of strings to a single string.
+    string encode(vector<string>& v) {
+        int n=v.size();
+        
         string s="";
-        for(int i=0;i<v.size();i++){
+        for(int i=0;i<n;i++){
             int len=v[i].length();
-            s+=to_string(len)+"$"+v[i];
+            
+            s += to_string(len) + "$" + v[i];
         }
+        
         return s;
     }
 
-    vector<string> decode(string &s) {
+    // Decodes a single string to a list of strings.
+    vector<string> decode(string s) {
         vector<string> v;
-        int n=s.length();
-        int i=0;
+        int n=s.length(), i=0;
+        
         while(i<n){
             int len=0;
-            while(i<n && s[i]!='$'){
-                len=len*10+(s[i]-'0');
-                i++;
-            }
+            while(i<n && isdigit(s[i])) len = len*10 + (s[i++]-'0');
+            
             i++;
-            string cur="";
-            while(i<n && len!=0){
-                cur+=s[i];
-                i++;
-                len--;
-            }
-            v.push_back(cur);
+            string sub="";
+            while(len--) sub+=s[i++];
+            
+            v.push_back(sub);
         }
+        
         return v;
     }
 };
