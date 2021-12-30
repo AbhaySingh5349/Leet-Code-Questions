b127ba9ebabddd611df5d404b585dc48095ed0b3
Abhay Singh <abhay5349singh@gmail.com>
2021-12-30 13:01:18 +0530

    Add files via upload

---
+++ Dynamic Programming/746. Min Cost Climbing Stairs.cpp
@@ -0,0 +1,23 @@
+Question Link: https://leetcode.com/problems/find-subsequence-of-length-k-with-the-largest-sum/
+
+class Solution {
+public:
+    
+    map<int,int> mp;
+    
+    int cost(vector<int> &a, int idx){
+        int n=a.size();
+        if(idx>=n) return 0;
+        
+        if(mp.find(idx)!=mp.end()) return mp[idx];
+        
+        int ans=a[idx];
+        ans += min(cost(a,idx+1),cost(a,idx+2));
+        
+        return mp[idx]=ans;
+    }
+    
+    int minCostClimbingStairs(vector<int> &a) {
+        return min(cost(a,0),cost(a,1));
+    }
+};
