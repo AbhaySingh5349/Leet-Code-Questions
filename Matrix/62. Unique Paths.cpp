aa590ec89192d7155c60c7b266058c8bcd33405f
Abhay Singh <abhay5349singh@gmail.com>
2022-01-10 00:16:22 +0530

    Update 62. Unique Paths.cpp

--- Matrix/62. Unique Paths.cpp
+++ Matrix/62. Unique Paths.cpp
@@ -1,5 +1,24 @@
 Question Link: https://leetcode.com/problems/capitalize-the-title/
 
+// Approach 1:
+
+class Solution {
+public:
+    
+    int uniquePaths(int m, int n) {
+        int dp[m][n];
+        for(int i=0, j=0;j<n;j++) dp[i][j]=1;
+        for(int j=0, i=0;i<m;i++) dp[i][j]=1;
+        
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++) dp[i][j]=dp[i-1][j] + dp[i][j-1];
+        }
+        return dp[m-1][n-1]; 
+    }
+};
+
+// Approach 2:
+
 class Solution {
 public:
     
@@ -13,14 +32,6 @@ public:
     }
     
     int uniquePaths(int m, int n) {
-    /*    int dp[m][n];
-        for(int i=0, j=0;j<n;j++) dp[i][j]=1;
-        for(int j=0, i=0;i<m;i++) dp[i][j]=1;
-        
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++) dp[i][j]=dp[i-1][j] + dp[i][j-1];
-        }
-        return dp[m-1][n-1]; */
         
         return ncr(m+n-2,min(n-1,m-1));
     }
