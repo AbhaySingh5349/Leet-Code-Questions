4e877b3505d17117e4ed90fbcfdb8134fee5339a
Abhay Singh <abhay5349singh@gmail.com>
2022-01-13 13:07:53 +0530

    Update 1168. Optimize Water Distribution in a Village.cpp

--- Graphs/1168. Optimize Water Distribution in a Village.cpp
+++ Graphs/1168. Optimize Water Distribution in a Village.cpp
@@ -1,4 +1,4 @@
-Question Link:https://leetcode.com/problems/minimum-cost-to-connect-sticks/
+Question Link: https://leetcode.com/problems/optimize-water-distribution-in-a-village/
 
 class Solution {
 public:
