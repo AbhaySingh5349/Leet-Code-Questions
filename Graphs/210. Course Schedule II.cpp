6bca023817f811f1bd2abfcf019c11a3f5823869
Abhay Singh <abhay5349singh@gmail.com>
2022-01-18 11:36:23 +0530

    Update 210. Course Schedule II.cpp

--- Graphs/210. Course Schedule II.cpp
+++ Graphs/210. Course Schedule II.cpp
@@ -1,4 +1,6 @@
-Question Link: https://leetcode.com/problems/minimum-cost-to-connect-sticks/
+Question Link: https://leetcode.com/problems/course-schedule-ii/
+
+// Approach 1:
 
 class Solution {
 public:
@@ -61,5 +63,44 @@ public:
         reverse(ans.begin(),ans.end());
         
         return ans;
+    }
+};
+
+// Approach 2:
+
+class Solution {
+public:
+    
+    vector<int> findOrder(int n, vector<vector<int>>& pre) {
+        vector<int> graph[n];
+        vector<int> indegree(n,0);
+        for(int i=0;i<pre.size();i++){
+            int u=pre[i][0], v=pre[i][1];
+            graph[v].push_back(u);
+            indegree[u]++;
+        }
+        
+        queue<int> q;
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+        int c=0;
+        vector<int> ans(n);
+        while(q.size()>0){
+            int src=q.front();
+            q.pop();
+            ans[c]=src;
+            c++;
+            
+            for(int i=0;i<graph[src].size();i++){
+                int child=graph[src][i];
+                indegree[child]--;
+                if(indegree[child]==0) q.push(child);
+            }
+        }
+        if(c<n) return {};
+        return ans; 
     }
 };
