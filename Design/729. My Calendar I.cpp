6739f3199aca6c45b1ab89285387dec41d05482e
Abhay Singh <abhay5349singh@gmail.com>
2022-02-02 15:23:40 +0530

    Add files via upload

---
+++ Design/729. My Calendar I.cpp
@@ -0,0 +1,25 @@
+Question Link: https://leetcode.com/problems/trapping-rain-water/
+
+class MyCalendar {
+public:
+    
+    map<int,int> mp;
+    
+    MyCalendar() {
+        mp.clear();
+    }
+    
+    bool book(int start, int end) {
+        auto it = mp.lower_bound(start);
+        
+        if(it!=mp.end() && it->first < end) return false;
+        
+        if(it!=mp.begin()){
+            it--;
+            if(it->second > start) return false;
+        }
+        
+        mp[start]=end;
+        return true;
+    }
+};
