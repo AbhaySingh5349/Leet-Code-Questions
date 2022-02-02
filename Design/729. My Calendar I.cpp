ade964d4f2626bf2081e75db4b84bb2c00a2cf11
Abhay Singh <abhay5349singh@gmail.com>
2022-02-02 15:43:44 +0530

    Update 729. My Calendar I.cpp

--- Design/729. My Calendar I.cpp
+++ Design/729. My Calendar I.cpp
@@ -1,5 +1,33 @@
 Question Link: https://leetcode.com/problems/my-calendar-i/
 
+// Approach 1:
+
+class MyCalendar {
+public:
+    
+    map<int,int> mp;
+    
+    MyCalendar() {
+        mp.clear();
+    }
+    
+    bool book(int start, int end) {
+        mp[start]++, mp[end]--;
+        
+        int count=0;
+        for(auto it : mp){
+            count += (it.second);
+            if(count>1){
+                mp[start]--, mp[end]++;
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+// Approach 2:
+
 class MyCalendar {
 public:
     
